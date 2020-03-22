faee70462357257499a07995de5bdc827ab2589b
Francisco Giordano <frangio.1@gmail.com>
2020-03-22 00:41:53 -0300

    reimplement in lua

---
+++ lua/ripgrep.lua
@@ -0,0 +1,120 @@
+local dkjson = require('dkjson')
+local api = vim.api
+local loop = vim.loop
+
+local Buffer = {}
+Buffer.__index = Buffer
+
+function init_buffer(buffer, query)
+  Buffer.new(buffer, query):init()
+end
+
+function setup_window(window)
+  api.nvim_win_set_option(window, 'number', false)
+end
+
+function Buffer.new(buffer, query)
+  local self = { buffer = buffer, query = query, data = {} }
+  setmetatable(self, Buffer)
+  return self
+end
+
+function Buffer:init()
+  api.nvim_buf_set_option(self.buffer, 'filetype', 'ripgrep')
+  api.nvim_buf_set_option(self.buffer, 'buftype', 'nofile')
+  api.nvim_buf_set_option(self.buffer, 'bufhidden', 'hide')
+  api.nvim_buf_set_option(self.buffer, 'swapfile', false)
+  api.nvim_buf_set_option(self.buffer, 'modifiable', false)
+  api.nvim_buf_set_option(self.buffer, 'modified', true)
+
+  spawn('rg', {'--json', '--', self:get_pattern()}, function (err, output)
+    local len = output:len()
+    local pos = 1
+    while pos <= len do
+      local obj, err
+      obj, pos = dkjson.decode(output, pos)
+      if obj and self[obj.type] then
+        self[obj.type](self, obj.data)
+      end
+    end
+  end)
+end
+
+function Buffer:get_pattern()
+  return self.query:match("rg://(.*)")
+end
+
+function Buffer:append(lines)
+  local start = self.appended and -1 or -2
+  self.appended = true
+  api.nvim_buf_set_option(self.buffer, 'modifiable', true)
+  api.nvim_buf_set_lines(self.buffer, start, -1, true, lines)
+  api.nvim_buf_set_option(self.buffer, 'modifiable', false)
+  return api.nvim_buf_line_count(self.buffer) - 1
+end
+
+function Buffer:begin(data)
+  local line = self:append({data.path.text})
+  api.nvim_buf_add_highlight(self.bufer, -1, 'rgFileName', line, 0, -1)
+end
+
+function Buffer:match(data)
+  local line = self:append(split_lines(data.lines.text))
+  for i, m in ipairs(data.submatches) do
+    api.nvim_buf_add_highlight(self.bufer, -1, 'rgMatch', line, m.start, m['end'])
+  end
+end
+
+Buffer['end'] = function (self)
+  self:append({''})
+end
+
+function spawn(cmd, args, callback)
+  local handle
+  local chunks = {}
+  local stdout = loop.new_pipe(false)
+
+  function on_read(err, chunk)
+    if err then
+      vim.schedule(function () callback(err) end)
+    elseif chunk then
+      table.insert(chunks, chunk)
+    end
+  end
+
+  function on_exit(code, signal)
+    stdout:read_stop()
+    stdout:close()
+    handle:close()
+    vim.schedule(function () callback(nil, table.concat(chunks)) end)
+  end
+
+  handle = loop.spawn(cmd, {
+    args = args,
+    stdio = {nil, stdout, nil},
+  }, on_exit)
+
+  loop.read_start(stdout, on_read)
+end
+
+function split_lines(str)
+  local lines = {}
+  local len = str:len()
+  local pos = 1
+  while pos <= len do
+    local b, e = str:find("\r?\n", pos)
+    if b then
+      table.insert(lines, str:sub(pos, b - 1))
+      pos = e + 1
+    else
+      table.insert(lines, str:sub(pos))
+      break
+    end
+  end
+  return lines
+end
+
+return {
+  init_buffer = init_buffer,
+  setup_window = setup_window,
+}
--- plugin/ripgrep.vim
+++ plugin/ripgrep.vim
@@ -1,21 +1,15 @@
-function! s:BufReadCmd(buffer, file)
-  call ripgrep#init_buffer(a:buffer, a:file)
-endfunction
-
-function! s:BufWinEnter()
-  call ripgrep#highlight_reset()
-endfunction
-
-function! s:WinNew()
-  " this autocmd is needed because BufWinEnter doesn't run for :split
-  call ripgrep#highlight_apply()
-endfunction
-
 augroup ripgrep
   autocmd!
-
-  autocmd BufReadCmd  rg://*  call <SID>BufReadCmd(str2nr(expand("<abuf>")), expand("<afile>"))
-
-  autocmd BufWinEnter *       call <SID>BufWinEnter()
-  autocmd WinNew      *       call <SID>WinNew()
+  autocmd BufReadCmd  rg://*  call <SID>init_buffer(expand("<abuf>"), expand("<afile>"))
+  autocmd BufWinEnter rg://*  call <SID>setup_window(win_getid())
 augroup end
+
+function! s:init_buffer(buffer, query)
+  call luaeval('require("ripgrep").init_buffer(_A.buffer, _A.query)', a:)
+endfunction
+
+function! s:setup_window(window)
+  call luaeval('require("ripgrep").setup_window(_A.window)', a:)
+endfunction
+
+command! -nargs=? Rg exec <q-mods> 'new rg://' . <q-args>
