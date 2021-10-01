2fed6ad68f866855ed0683fe498b91dbe83a829c
Francisco Giordano <frangio.1@gmail.com>
2021-10-01 20:10:00 -0300

    use built in lua-cjson if available

--- lua/ripgrep/utils/spawn_json_producer.lua
+++ lua/ripgrep/utils/spawn_json_producer.lua
@@ -1,10 +1,11 @@
-local dkjson = require('ripgrep.dkjson')
 local spawn = require('ripgrep.utils.spawn')
 local each_line = require('ripgrep.utils.each_line')
 
+local json_decode = (vim.json and vim.json.decode) or require('ripgrep.dkjson').decode
+
 local function spawn_json_producer(cmd, args, visitors)
   local callback = each_line(function (line)
-    local obj = dkjson.decode(line)
+    local obj = json_decode(line)
     if visitors[obj.type] then
       visitors[obj.type](obj.data)
     end
