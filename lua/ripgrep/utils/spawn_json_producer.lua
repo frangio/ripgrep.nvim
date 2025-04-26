ff69a0958a23ef19a65fe9117b8b3a044493a624
Francisco Giordano <fg@frang.io>
2025-04-25 23:43:52 -0300

    remove dkjson

--- lua/ripgrep/utils/spawn_json_producer.lua
+++ lua/ripgrep/utils/spawn_json_producer.lua
@@ -1,11 +1,9 @@
 local spawn = require('ripgrep.utils.spawn')
 local each_line = require('ripgrep.utils.each_line')
 
-local json_decode = (vim.json and vim.json.decode) or require('ripgrep.dkjson').decode
-
 local function spawn_json_producer(cmd, args, visitors)
   local callback = each_line(function (line)
-    local obj = json_decode(line)
+    local obj = vim.json.decode(line)
     if visitors[obj.type] then
       visitors[obj.type](obj.data)
     end
